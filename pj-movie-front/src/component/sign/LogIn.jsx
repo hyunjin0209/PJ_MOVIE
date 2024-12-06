f84ddb097d197b24fe91284a7752a8eb5cf869e0
BOOK-LVSQ3T5EMS\huji0 <huji0209@naver.com>
2024-12-06 16:41:39 +0900

    로그인시 버튼 로그아웃으로 변경 및 로그아웃이 세션 데이터 삭제

--- pj-movie-front/src/component/sign/LogIn.jsx
+++ pj-movie-front/src/component/sign/LogIn.jsx
@@ -35,7 +35,7 @@ export default function LogIn() {
         sessionStorage.setItem("id", response.data.pmUserId);
         sessionStorage.setItem("email", response.data.pmUserEmail);
         sessionStorage.setItem("phone number", response.data.pmUserPhone);
-        nav("/NowScreening");
+        nav("/mainpage");
       } else {
         alert("아이디 또는 비밀번호가 다릅니다");
       }
