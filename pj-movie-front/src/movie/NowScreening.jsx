7efd505327275210b4ebd93dbf7ce26f547ba97e
wjsquddh12345 <wjsquddh12345@gmail.com>
2024-12-31 14:56:59 +0900

    상영중인 영화, 로그인페이지 css입히기 및 로그인시 헤더 버튼명 변경

--- pj-movie-front/src/movie/NowScreening.jsx
+++ pj-movie-front/src/movie/NowScreening.jsx
@@ -1,16 +1,22 @@
 import { useNavigate } from "react-router-dom";
 import { MovieData } from "../image.js/MovieData/MovieData";
-import "../css/MainHome.css";
+import "c:/Users/82108/Desktop/B.O-project/PJ_MOVIE/pj-movie-front/src/css/MainHome.css";
 import { useState } from "react";
 import Pagination from "react-js-pagination";
 export default function NowScreening() {
   const nav = useNavigate();
+  const [sessionDate, setsessionDate] = useState(sessionStorage.getItem("id"));
 
   const [page, setPage] = useState(1);
-  let itemsPerPage = 2;
+  let itemsPerPage = 3;
 
   const handlePageChange = (page) => {
     setPage(page);
+  };
+
+  const logout = () => {
+    sessionStorage.clear();
+    setsessionDate();
   };
 
   // 페이지 변경 시 currentMovies를 업데이트
@@ -20,14 +26,26 @@ export default function NowScreening() {
   return (
     <div className="main-home">
       <header className="header">
-        <h1>영화 홈페이지</h1>
+        <h1 onClick={() => nav("/")}>영화 홈페이지</h1>
         <div className="button-container">
-          <button onClick={() => nav("/Login")}>로그인</button>
-          <button onClick={() => nav("/UserJoin")}>회원가입</button>
+          <div>
+            {sessionDate ? (
+              <button onClick={logout}>로그아웃</button>
+            ) : (
+              <button onClick={() => nav("/Login")}>로그인</button>
+            )}
+          </div>
+          <div>
+            {sessionDate ? (
+              <button onClick={() => nav("/MyPage")}>내정보</button>
+            ) : (
+              <button onClick={() => nav("UserJoin")}>회원가입</button>
+            )}
+          </div>
           <button onClick={() => nav("/BoardList")}>게시판</button>
         </div>
       </header>
-
+      <h1>상영 중 영화 목록</h1>
       <div className="movie-grid">
         {movies.map((data, index) => (
           <div key={index} className="movie-item">
@@ -36,6 +54,10 @@ export default function NowScreening() {
             <img src={data.Image} className="movie-image" />
           </div>
         ))}
+      </div>
+
+      {/* 페이지네이션을 movie-grid 바깥에 배치 */}
+      <div className="pagination-container">
         <Pagination
           activePage={page} // 현재 페이지
           itemsCountPerPage={itemsPerPage} // 한 페이지랑 보여줄 아이템 갯수
